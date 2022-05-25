deecca8729a305ce9c3450f244d6a61954bcbb18
Nicke Nicrad <nickenicrad@gmail.com>
2022-05-25 06:56:22 +0200

    db backup added in folder called db

--- routes/auth.php
+++ routes/auth.php
@@ -6,17 +6,18 @@ use App\Http\Controllers\Auth\EmailVerificationNotificationController;
 use App\Http\Controllers\Auth\EmailVerificationPromptController;
 use App\Http\Controllers\Auth\NewPasswordController;
 use App\Http\Controllers\Auth\PasswordResetLinkController;
+use App\Http\Controllers\Auth\RegisteredUserController;
 use App\Http\Controllers\Auth\VerifyEmailController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('guest')->group(function () {
 
-    // Route::get('/register', [RegisteredUserController::class, 'create'])
-    //     ->middleware('guest')
-    //     ->name('register');
+    Route::get('/register', [RegisteredUserController::class, 'create'])
+        ->middleware('guest')
+        ->name('register');
 
-    // Route::post('/register', [RegisteredUserController::class, 'store'])
-    //     ->middleware('guest');
+    Route::post('/register', [RegisteredUserController::class, 'store'])
+        ->middleware('guest');
 
     Route::get('login', [AuthenticatedSessionController::class, 'create'])
         ->name('login');
